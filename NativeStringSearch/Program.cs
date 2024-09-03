cea730dffb2c452139a465148c08715be68cc4cf
Stefan Panevski <I35199@3ecorp.com>
2024-09-03 14:20:25 +0300

    - add NativeStringSearch task

---
+++ NativeStringSearch/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NativeStringSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringSearch("lorie Loled", "lol");
+
+            static void StringSearch(string longString, string shortString)
+            {
+                int matches = 0;
+                longString = longString.ToLower();
+                shortString = shortString.ToLower();
+
+                for (int i = 0; i < longString.Length; i++)
+                {
+                    for (int j = 0; j < shortString.Length; j++)
+                    {
+                       if (shortString[j]!= longString[i+j]) break;
+                       if (j == shortString.Length-1) matches++;
+                    }
+                }
+
+                Console.WriteLine(matches);
+            }
+        }
+    }
+}
