b49dba18b8f5163006e5c84e8b843bca87320724
Stefan Panevski <I35199@3ecorp.com>
2024-09-16 09:11:42 +0300

    - formatting and add info about the problem solved in NativeStringSearch

--- NativeStringSearch/Program.cs
+++ NativeStringSearch/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            //A method which solves the problem of finding how many times a string is contained within a larger string. The method accepts the longer string and then the shorter string that we want to find
+            //if it is conatined (and if yes - how many times) within the longer string.
             StringSearch("lorie Loled", "lol");
+            StringSearch("olelele malelelelei", "le");
 
             static void StringSearch(string longString, string shortString)
             {
@@ -16,8 +19,8 @@
                 {
                     for (int j = 0; j < shortString.Length; j++)
                     {
-                       if (shortString[j]!= longString[i+j]) break;
-                       if (j == shortString.Length-1) matches++;
+                        if (shortString[j] != longString[i + j]) break;
+                        if (j == shortString.Length - 1) matches++;
                     }
                 }
 
