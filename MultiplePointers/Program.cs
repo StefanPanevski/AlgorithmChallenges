3089fb58c174f78d15e98494d6dab9aee80573a8
Stefan Panevski <I35199@3ecorp.com>
2024-09-30 10:23:51 +0300

    - improve result display for multiple pointers task (unique value algorithm)

--- MultiplePointers/Program.cs
+++ MultiplePointers/Program.cs
@@ -9,15 +9,7 @@
              * countUniqueValues([1,1,1,1,1,2]) // 2
              * countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
              * countUniqueValues([]) // 0
-             * countUniqueValues([-2,-1,-1,0,1]) // 4
-             * Time Complexity - O(n)
-             * 
-             * Space Complexity - O(n
-             * 
-             * 
-             * Bonus
-             * 
-             * You must do this with constant or O(1) space and O(n) time.*/
+             * countUniqueValues([-2,-1,-1,0,1]) // 4 */
 
             string input = Console.ReadLine();
 
@@ -62,7 +54,7 @@
                 }
 
                 Console.WriteLine();
-                Console.WriteLine(uniqueValue.Count);
+                Console.WriteLine($"The number of unique values is: {uniqueValue.Count}");
             }
         }
     }
