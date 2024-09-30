a2bd894828db9b7d3fcf7464bee0c0740d4ffddb
Stefan Panevski <I35199@3ecorp.com>
2024-09-30 11:21:42 +0300

    - imrpove breaking out of loops if array is sorted before exepcted max swaps needed

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //TODO: Implement a bubble sort algorithm here
-            BubbleSort([25, 3, 100, 7, 4, 58, 18, 30, 5, 10, 2]);
+            //BubbleSort([25, 3, 100, 7, 4, 58, 18, 30, 5, 10, 2]);
+            BubbleSort([10, 2, 3, 4, 5, 6, 7]);
 
             static int[] BubbleSort(int[] sortArray)
             {
@@ -16,23 +17,32 @@
                 }
                 Console.WriteLine();
 
+                bool swapValue = false;
+
                 for (int i = sortArray.Length - 1; i > 0; i--)
                 {
                     Console.WriteLine("New outer loop iteration:");
-
+                    swapValue = false;
                     for (int j = 0; j < i; j++)
                     {
                         if (sortArray[j] > sortArray[j + 1])
                         {
                             //swap values to sort the array
                             SwapValues(sortArray, j);
+                            swapValue = true;
                         }
-
+                        
                         foreach (var item in sortArray)
                         {
                             Console.Write($"{item} ");
                         }
                         Console.WriteLine();
+                    }
+
+                    if (swapValue == false)
+                    {
+                        Console.WriteLine("Breaking out as no swaps were made last run.");
+                        break;
                     }
                 }
 
